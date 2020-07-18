28b81f695624272249aa70eefbe1bd768831c09d
lestercardoz11 <lestercardoz11@gmail.com>
2020-07-18 16:14:32 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Lester Cardoz - Broken Rock Paper Scissors</title>
+<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+<div class="container">
+<h1>Rock Paper Scissors</h1>
+<p>
+<a href="login.php">Please Log In</a>
+</p>
+<p>
+Attempt to go to 
+<a href="game.php">game.php</a> without logging in - it should fail with an error message.
+</div>
+</body>
+
